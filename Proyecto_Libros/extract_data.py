3b9f9f3bb35a8db9f63533ab517ea8c6d02a1db2
miniblue0 <agustinezequielnievas69@gmail.com>
2024-12-13 22:59:29 -0300

    Cree el script para extraer los datos y 2 nuevos archivos donde se almacenaran el proceso de carga a Azure y un Json con datos crudos

--- Proyecto_Libros/extract_data.py
+++ Proyecto_Libros/extract_data.py
@@ -1,3 +1,31 @@
 #en este archivo debe estar el proceso para extraer y almacenar en el Data Lake sin ninguna transformacion.
-from config import GOOGLE_BOOKS_API_KEY
-from azure.storage.filedatalake import DataLakeServiceClient
+#from azure.storage.filedatalake import DataLakeServiceClient
+import requests
+import json
+#from config import GOOGLE_API_KEY
+GOOGLE_API_KEY = 'AIzaSyBaFXJ-cyL8THx4VcG_jsbLaUn13uc5Xng'
+
+
+
+##hago la peticion de siempre a la api pero use un maximo de libros (para poder ver mejor los datos) si cambian max_results les devuelve mas o menos libros
+def extract_data(query = 'book', max_results = 1):
+    url = f"https://www.googleapis.com/books/v1/volumes?q={query}&key={GOOGLE_API_KEY}&maxResults={max_results}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        books_data = response.json().get("items", [])
+        print(f"Se obtuvieron {len(books_data)} libros.")
+        return books_data
+    else:
+        raise Exception(f"Error al obtener datos: {response.status_code}")
+
+
+
+##ahora guardo los libros en un archivo json aparte para luego subirlo al dataLake con azure
+def save_raw_data(books_data, output_path):
+    with open(output_path, 'w') as f:
+        json.dump(books_data, f, indent=4)
+    print(f"Datos crudos guardados en {output_path}")
+
+
+books = extract_data(query = 'book', max_results = 1)
+save_raw_data(books, "Proyecto_Libros/raw_books.json")
