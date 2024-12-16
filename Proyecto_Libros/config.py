52da7282d780c4efe248bab2fa9c44597152ee85
miniblue0 <agustinezequielnievas69@gmail.com>
2024-12-17 00:47:10 +0100

    'cree la funcion para cargar los datos en crudo a un bucket S3'

--- Proyecto_Libros/config.py
+++ Proyecto_Libros/config.py
@@ -1,12 +1,19 @@
-# config.py
+#config.py
 import os
 from dotenv import load_dotenv
 
-# Cargar las variables de entorno desde el archivo .env
+#Cargar las variables de entorno desde el archivo .env
 load_dotenv()
 
-# Obtener las credenciales desde las variables de entorno
+#Obtengo las credenciales desde las variables de entorno
+
+#Credenciales de AWS
+AWS_ACCESS_KEY_ID = os.getenv("SECRET_ID_KEY")
+AWS_SECRET_ACCESS_KEY = os.getenv("SECRET_ACCESS_KEY")
+S3_BUCKET_NAME = os.getenv("BUCKET_NAME")
+
+#Credenciales de la api
 GOOGLE_BOOKS_API_KEY = os.getenv("GOOGLE_BOOKS_API_KEY")
-DATALAKE_ACCOUNT_NAME = os.getenv("DATALAKE_ACCOUNT_NAME")
-DATALAKE_ACCOUNT_KEY = os.getenv("DATALAKE_ACCOUNT_KEY")
+
+#Credenciales de la base de datos
 SQL_SERVER_CONNECTION_STRING = os.getenv("SQL_SERVER_CONNECTION_STRING")
